648317b9d4b58874007303fd7392c09436b10e46
Nathaniel Davila <nat.admin@inventati.org>
2022-03-01 19:24:41 -0800

    Change TCP to any.

--- main.tf
+++ main.tf
@@ -109,7 +109,7 @@ resource "azurerm_network_security_group" "example" {
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "Tcp"
+    protocol                   = "*"
     source_port_range          = "*"
     destination_port_range     = "*"
     source_address_prefix      = "*"
